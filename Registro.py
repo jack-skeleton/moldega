e8df20321d53976f03fceeab0c998ed2a51a128b
jack-skeleton <65924958+jack-skeleton@users.noreply.github.com>
2020-05-25 18:52:54 -0500

    Create Registro.py

---
+++ Registro.py
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="Registrarse">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+	<section class="form_check_in">
+        
+        <input class="framwork" type="text" name="Nombres" id="nombres" placeholder="ingrese su nombre">
+        <input class="framwork" type="text" name="Apellido" id="apellido" placeholder="ingrese su apellido">
+        <input class="framwork" type="email" name="correo" id="correo" placeholder="crear una contraseña">
+        <input class="framwork" type="password" name="password" id="password" placeholder="cree una contraseña">
+        <p>Estoy de acuerdo con <a href="#">Terminos y Condiciones</a></p>
+        <input  class="button" type="submit" value="Registrar">
+        <p><a href="#">Iniciar sesión</a></p>
+    
+    
+    </section> 
+	
+	<footer>
+		<p>--------</p>
+	</footer>
+</body>
+</html>
